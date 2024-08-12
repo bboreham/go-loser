390fb8173aa4a53c873f92bb8fe5b7b42c9fab76
Bryan Boreham <bjboreham@gmail.com>
2024-08-12 14:39:31 +0100

    Clean up iterators a bit
    Use more canonical names; move all work to the iterator function.
    
    Call the stop() function.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- tree.go
+++ tree.go
@@ -10,24 +10,15 @@ type Lesser[T any] interface {
 	Less(T) bool
 }
 
-type Iterable[E Lesser[E]] interface {
-	Iter() iter.Seq[E]
+type Sequence[E Lesser[E]] interface {
+	All() iter.Seq[E]
 }
 
-func New[E Lesser[E]](sequences []Iterable[E], maxVal E) *Tree[E] {
-	nSequences := len(sequences)
+func New[E Lesser[E]](sequences []Sequence[E], maxVal E) *Tree[E] {
 	t := Tree[E]{
-		maxVal: maxVal,
-		nodes:  make([]node[E], nSequences*2),
-	}
-	for i, s := range sequences {
-		next, stop := iter.Pull(s.Iter())
-		t.nodes[i+nSequences].next = next
-		t.nodes[i+nSequences].stop = stop
-		t.moveNext(i + nSequences) // Must call Next on each item so that At() has a value.
-	}
-	if nSequences > 0 {
-		t.nodes[0].index = -1 // flag to be initialized on first call to Next().
+		maxVal:    maxVal,
+		nodes:     make([]node[E], len(sequences)*2),
+		sequences: sequences,
 	}
 	return &t
 }
@@ -36,15 +27,15 @@ func New[E Lesser[E]](sequences []Iterable[E], maxVal E) *Tree[E] {
 // We store M leaf nodes in positions M...2M-1, and M-1 internal nodes in positions 1..M-1.
 // Node 0 is a special node, containing the winner of the contest.
 type Tree[E Lesser[E]] struct {
-	maxVal E
-	nodes  []node[E]
+	maxVal    E
+	nodes     []node[E]
+	sequences []Sequence[E]
 }
 
 type node[E Lesser[E]] struct {
 	index int              // This is the loser for all nodes except the 0th, where it is the winner.
 	value E                // Value copied from the loser node, or winner for node 0.
 	next  func() (E, bool) // Only populated for leaf nodes.
-	stop  func()           // Only populated for leaf nodes.
 }
 
 func (t *Tree[E]) moveNext(index int) bool {
@@ -58,24 +49,22 @@ func (t *Tree[E]) moveNext(index int) bool {
 	return false
 }
 
-func (t *Tree[E]) Iter() iter.Seq[E] {
+func (t *Tree[E]) All() iter.Seq[E] {
 	return func(yield func(E) bool) {
-		nodes := t.nodes
-		if len(nodes) == 0 {
+		if len(t.nodes) == 0 {
 			return
 		}
-		var cont bool
-		t.initialize()
-		if nodes[nodes[0].index].index != -1 {
-			cont = yield(nodes[0].value)
+		for i, s := range t.sequences {
+			next, stop := iter.Pull(s.All())
+			t.nodes[i+len(t.sequences)].next = next
+			defer stop()
+			t.moveNext(i + len(t.sequences)) // Call next() on each item to get the first value.
 		}
-		for cont {
-			t.moveNext(nodes[0].index)
-			t.replayGames(nodes[0].index)
-			if nodes[nodes[0].index].index == -1 {
-				break
-			}
-			cont = yield(nodes[0].value)
+		t.initialize()
+		for t.nodes[t.nodes[0].index].index != -1 &&
+			yield(t.nodes[0].value) {
+			t.moveNext(t.nodes[0].index)
+			t.replayGames(t.nodes[0].index)
 		}
 	}
 }
--- tree_test.go
+++ tree_test.go
@@ -17,7 +17,7 @@ func NewList[E loser.Lesser[E]](list ...E) *List[E] {
 	return &List[E]{list: list}
 }
 
-func (it *List[E]) Iter() iter.Seq[E] {
+func (it *List[E]) All() iter.Seq[E] {
 	return func(yield func(E) bool) {
 		for _, i := range it.list {
 			yield(i)
@@ -25,12 +25,12 @@ func (it *List[E]) Iter() iter.Seq[E] {
 	}
 }
 
-func checkIterablesEqual[E loser.Lesser[E]](t *testing.T, a loser.Iterable[E], b loser.Iterable[E]) {
+func checkIterablesEqual[E loser.Lesser[E]](t *testing.T, a loser.Sequence[E], b loser.Sequence[E]) {
 	t.Helper()
 	count := 0
-	next, stop := iter.Pull(b.Iter())
+	next, stop := iter.Pull(b.All())
 	defer stop()
-	for va := range a.Iter() {
+	for va := range a.All() {
 		count++
 		vb, ok := next()
 		if !ok {
@@ -48,7 +48,7 @@ func checkIterablesEqual[E loser.Lesser[E]](t *testing.T, a loser.Iterable[E], b
 func TestMerge(t *testing.T) {
 	tests := []struct {
 		name string
-		args []loser.Iterable[Uint64]
+		args []loser.Sequence[Uint64]
 		want *List[Uint64]
 	}{
 		{
@@ -57,37 +57,37 @@ func TestMerge(t *testing.T) {
 		},
 		{
 			name: "one list",
-			args: []loser.Iterable[Uint64]{NewList[Uint64](1, 2, 3, 4)},
+			args: []loser.Sequence[Uint64]{NewList[Uint64](1, 2, 3, 4)},
 			want: NewList[Uint64](1, 2, 3, 4),
 		},
 		{
 			name: "two lists",
-			args: []loser.Iterable[Uint64]{NewList[Uint64](3, 4, 5), NewList[Uint64](1, 2)},
+			args: []loser.Sequence[Uint64]{NewList[Uint64](3, 4, 5), NewList[Uint64](1, 2)},
 			want: NewList[Uint64](1, 2, 3, 4, 5),
 		},
 		{
 			name: "two lists, first empty",
-			args: []loser.Iterable[Uint64]{NewList[Uint64](), NewList[Uint64](1, 2)},
+			args: []loser.Sequence[Uint64]{NewList[Uint64](), NewList[Uint64](1, 2)},
 			want: NewList[Uint64](1, 2),
 		},
 		{
 			name: "two lists, second empty",
-			args: []loser.Iterable[Uint64]{NewList[Uint64](1, 2), NewList[Uint64]()},
+			args: []loser.Sequence[Uint64]{NewList[Uint64](1, 2), NewList[Uint64]()},
 			want: NewList[Uint64](1, 2),
 		},
 		{
 			name: "two lists b",
-			args: []loser.Iterable[Uint64]{NewList[Uint64](1, 2), NewList[Uint64](3, 4, 5)},
+			args: []loser.Sequence[Uint64]{NewList[Uint64](1, 2), NewList[Uint64](3, 4, 5)},
 			want: NewList[Uint64](1, 2, 3, 4, 5),
 		},
 		{
 			name: "two lists c",
-			args: []loser.Iterable[Uint64]{NewList[Uint64](1, 3), NewList[Uint64](2, 4, 5)},
+			args: []loser.Sequence[Uint64]{NewList[Uint64](1, 3), NewList[Uint64](2, 4, 5)},
 			want: NewList[Uint64](1, 2, 3, 4, 5),
 		},
 		{
 			name: "three lists",
-			args: []loser.Iterable[Uint64]{NewList[Uint64](1, 3), NewList[Uint64](2, 4), NewList[Uint64](5)},
+			args: []loser.Sequence[Uint64]{NewList[Uint64](1, 3), NewList[Uint64](2, 4), NewList[Uint64](5)},
 			want: NewList[Uint64](1, 2, 3, 4, 5),
 		},
 	}
@@ -107,7 +107,7 @@ func (u Uint64) Less(other Uint64) bool {
 
 func BenchmarkMerge(b *testing.B) {
 	var lists []*List[Uint64]
-	var iterables []loser.Iterable[Uint64]
+	var iterables []loser.Sequence[Uint64]
 	var data [][]Uint64
 
 	// Create 10000 Lists, so that all memory allocation is done before starting the benchmark.
@@ -136,7 +136,7 @@ func BenchmarkMerge(b *testing.B) {
 }
 
 func consume[E loser.Lesser[E]](t *loser.Tree[E]) {
-	for v := range t.Iter() {
+	for v := range t.All() {
 		_ = v
 	}
 }
